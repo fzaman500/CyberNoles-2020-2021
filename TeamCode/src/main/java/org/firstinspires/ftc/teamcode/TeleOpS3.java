9725f8c69047a04ea3aba7fa01ced5e408ac9f78
prave <prav.perera@gmail.com>
2020-10-27 14:47:09 -0400

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpS3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpS3.java
@@ -39,7 +39,7 @@ public class TeleOpS3 extends LinearOpMode {
         telemetry.update();
 
         waitForStart();
-        //Hello, this is Withanage.
+        //Hello, this is Withanage...
 
         leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
