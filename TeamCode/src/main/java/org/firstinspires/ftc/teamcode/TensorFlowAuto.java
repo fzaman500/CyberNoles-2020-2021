1643e1d6ded7f7516d767cb012821a291e64e20e
Fatema Zaman <fzaman500@gmail.com>
2021-02-02 16:01:55 -0500

    tensorflow adjustments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowAuto.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -50,9 +51,9 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
+@Autonomous(name = "TensorFlow Auto")
 //@Disabled
-public class TensorFlowObjectDetection extends LinearOpMode {
+public class TensorFlowAuto extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Quad";
     private static final String LABEL_SECOND_ELEMENT = "Single";
